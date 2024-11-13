597556e0f75f503ea8e220d195308e6c9399f0df
anderwya000 <anderwya000@students.elkhorn.k12.wi.us>
2024-11-13 07:59:35 -0600

    not much working maybe store numbers in list

--- main.py
+++ main.py
@@ -1,27 +1,51 @@
 from rich.prompt import *
 from rich import print
-max = IntPrompt.ask('What is the maximum range to search?', default=10000)
-name = Prompt.ask('Name? ')
-print('Run each command, then tell if it returned the name.')
-print('/w @s @e[name="', name, '",x=0,y=-140,z=0,dx=', max, ',dy=512,dz=', max, ']', sep='')
+max = IntPrompt.ask('What is the maximum range to search?', default=5000)
+name = Prompt.ask('What is the target\'s name? ')
 
-if Confirm.ask("Did it return the name?"):
-    xaxis = ''
-    zaxis = ''
-else:
-    print('/w @s @e[name="', name, '",x=0,y=-140,z=0,dx=', max, ',dy=512,dz=-', max, ']', sep='')
-    if Confirm.ask("Did it return the name?"):
-        xaxis = ''
-        zaxis = '-'
+def find_axes():
+    global x_axis
+    global z_axis
+    print('Run each command, then tell if it returned the name.')
+    print('/w @s @e[name="', name, '",x=0,y=-140,z=0,dx=', max, ',dy=512,dz=', max, ']', sep='')
+    if Confirm.ask("Did it return the name?", default=False):
+        x_axis = ''
+        z_axis = ''
     else:
-        print('/w @s @e[name="', name, '",x=0,y=-140,z=0,dx=-', max, ',dy=512,dz=-', max, ']', sep='')
-        if Confirm.ask("Did it return the name?"):
-            xaxis = '-'
-            zaxis = '-'
+        print('/w @s @e[name="', name, '",x=0,y=-140,z=0,dx=', max, ',dy=512,dz=-', max, ']', sep='')
+        if Confirm.ask("Did it return the name?", default=False):
+            x_axis = ''
+            z_axis = '-'
         else:
-            print('/w @s @e[name="', name, '",x=0,y=-140,z=0,dx=-', max, ',dy=512,dz=', max, ']', sep='')
-            if Confirm.ask("Did it return the name?"):
-                xaxis = ''
-                zaxis = ''
+            print('/w @s @e[name="', name, '",x=0,y=-140,z=0,dx=-', max, ',dy=512,dz=-', max, ']', sep='')
+            if Confirm.ask("Did it return the name?", default=False):
+                x_axis = '-'
+                z_axis = '-'
             else:
-                print('Try again. The target might be near one of the axes')
+                print('/w @s @e[name="', name, '",x=0,y=-140,z=0,dx=-', max, ',dy=512,dz=', max, ']', sep='')
+                if Confirm.ask("Did it return the name?", default=False):
+                    x_axis = '-'
+                    z_axis = ''
+                else:
+                    print('Try again. The target might be near one of the axes')
+                    find_axes()
+
+
+def find_x_coordinate():
+    global x_max
+    global x_min
+    global x
+    x_min = 0
+    x_max = max
+    print('Run each command and reply if the target was found.')
+    print('/w @s @e[name="', name, '", x=0, y=-140, z=0, dx=', x_axis, x_max, ', dy=512, dz=', z_axis, max, ']', sep='')
+    if Confirm.ask("Did it return the name?", default=True):
+        asdf=0
+    else:
+        asdf=0
+    print(x_max)
+    print(x_min)
+    # x_max = (x_max + x_min) / 2
+
+find_axes()
+find_x_coordinate()
